91dc8464cb1c4d514dc998f3999570803c40b257
fr9nzn@gmail.com <fr9nzn@gmail.com>
2025-08-24 18:04:30 +0800

    feat: refactor server.js for improved CORS handling and static file serving; add axios configuration for dynamic base URL

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from 'cors';
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js"
 import { connectDB } from "./config/db.js";
@@ -11,13 +12,17 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000
-
+const __dirname = path.resolve()
 
 
 //Middlewares
-app.use(cors({
-    origin: "http://localhost:5173"
-}));
+
+if(process.env.NODE_ENV !== "production"){
+
+    app.use(cors({
+        origin: "http://localhost:5173"
+    }));
+}
 app.use(express.json()); //parses JSON bodies req.body
 app.use(rateLimiter);
 
@@ -29,6 +34,14 @@ app.use((req, res, next) => {
 
 //Routes
 app.use("/api/notes", notesRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*", (req, res) =>{
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+})
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
