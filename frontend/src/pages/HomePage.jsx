ac7945550ce4db694ad2201032919f5977579b50
fr9nzn@gmail.com <fr9nzn@gmail.com>
2025-08-24 16:48:53 +0800

    feat: implement CreatePage for note creation and refactor API calls to use custom axios instance

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import Navbar from "../components/Navbar"
 import RateLimitedUI from '../components/RateLimitedUI';
 import { useEffect } from 'react';
-import axios from 'axios';
+import api from "../lib/axios"
 import { toast } from 'react-hot-toast';
 import NoteCard from '../components/NoteCard';
 
@@ -16,7 +16,7 @@ const HomePage = () => {
     const fetchNotes = async () => {
 
       try {
-        const res = await axios.get("http://localhost:5001/api/notes");
+        const res = await api.get("/notes");
         console.log(res.data);
         setNotes(res.data)
         setIsRateLimited(false)
